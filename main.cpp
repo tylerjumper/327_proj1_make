790af54f08877a16afb1b4cb737a1022af6ce3fd
tylerjumper <tyler.jumper.16@cnu.edu>
2019-09-12 15:55:49 -0400

    let's try this again, .o files added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 //#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
+#include <string.h>
 #include "myfunc.h"
+
+
 
 int main()
 {
--- myfunc.cpp
+++ myfunc.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "myfunc.h"
 
+
 std::string func()
 {
         return "hello";
